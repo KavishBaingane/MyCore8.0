7e84ffe85efbf242db09ec7483d63f48254ddab2
Kavish-Evince <kavish@evincedev.com>
2024-08-07 19:04:07 +0530

    master:http request
    master:http request

--- MyCore8.0/Program.cs
+++ MyCore8.0/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    string Path = context.Request.Path;
+    string Method = context.Request.Method;
+
+
+    context.Response.Headers["Content-Type"] = "text/html";
+    await context.Response.WriteAsync($"<p>{Path}</p>");
+    await context.Response.WriteAsync($"<p>{Method}</p>");
+});
 
 app.Run();
