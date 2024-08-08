1f7381cf3ce861dcaa96127c108965b9ca718ced
Kavish-Evince <kavish@evincedev.com>
2024-08-08 11:12:42 +0530

    master:Request headers
    Request headers

--- MyCore8.0/Program.cs
+++ MyCore8.0/Program.cs
@@ -3,13 +3,18 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    string Path = context.Request.Path;
-    string Method = context.Request.Method;
 
-
-    context.Response.Headers["Content-Type"] = "text/html";
-    await context.Response.WriteAsync($"<p>{Path}</p>");
-    await context.Response.WriteAsync($"<p>{Method}</p>");
+    if (context.Request.Method == "GET")
+    {
+        context.Response.Headers["Content-Type"] = "text/html";
+        if (context.Request.Headers.ContainsKey("User-Agent"))
+        {
+            string userAgent = context.Request.Query["User-Agent"];
+            await context.Response.WriteAsync($"<p>{userAgent}</p>");
+        }
+    }
+   
+    
 });
 
 app.Run();
